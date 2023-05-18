de9323e8e09d3ea47479a7b45c08c28fcc48e3d6
sayaleesalunke <sayaleesalunke@gmail.com>
2023-05-18 13:36:33 +0530

    Initial commit

---
+++ src/main/java/com/test/project/stockanalysis/StockanalysisApplication.java
@@ -0,0 +1,13 @@
+package com.test.project.stockanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockanalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/project/stockanalysis/StockanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.project.stockanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
